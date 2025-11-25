54d931179f03979fa85bcba2413a71d8075f1c7b
Diego Morales <diemoret17@gmail.com>
2025-11-25 00:08:46 -0600

    blue0

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
         <div class="container">
           <h1>${APP_COLOR} Environment</h1>
           <p>Hello World!</p>
-          <p>Deployment: Blue/Green Strategy</p>
+          <p>Deployment: Blue Strategy</p>
         </div>
       </body>
     </html>
